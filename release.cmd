2c6d9d28213699f0f63425352f3b1223706cc10f
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-02-20 03:16:53 +0100

    improved release batch script to clean directory first

--- release.cmd
+++ release.cmd
@@ -1,4 +1,7 @@
+set ProjectName=xObsAsyncImageSource
 mkdir release\obs-plugins\64bit
-mkdir release\data\obs-plugins\xObsAsyncImageSource\locale
-move /Y publish\* release\obs-plugins\64bit\
-copy /Y locale\* release\data\obs-plugins\xObsAsyncImageSource\locale
+mkdir release\data\obs-plugins\%ProjectName%\locale
+del /F /S /Q release\obs-plugins\64bit\*
+del /F /S /Q release\data\obs-plugins\%ProjectName%\locale\*
+copy /Y publish\* release\obs-plugins\64bit\
+copy /Y locale\* release\data\obs-plugins\%ProjectName%\locale

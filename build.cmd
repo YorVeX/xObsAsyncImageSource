c4a474150dff148bfb7d9dd9d993272ac6529c91
YorVeX <528974+YorVeX@users.noreply.github.com>
2023-02-25 07:42:02 +0100

    Organized script structure into sub folders, included WSL preparation and WSL based build script for Linux x64 on a Windows system.

--- build.cmd
+++
@@ -1,2 +0,0 @@
-dotnet publish -c Release -o publish -r win-x64 /p:DefineConstants=WINDOWS /p:NativeLib=Shared /p:SelfContained=true
-pause
--- release.cmd
+++
@@ -1,7 +0,0 @@
-set ProjectName=xObsAsyncImageSource
-mkdir release\obs-plugins\64bit
-mkdir release\data\obs-plugins\%ProjectName%\locale
-del /F /S /Q release\obs-plugins\64bit\*
-del /F /S /Q release\data\obs-plugins\%ProjectName%\locale\*
-copy /Y publish\* release\obs-plugins\64bit\
-copy /Y locale\* release\data\obs-plugins\%ProjectName%\locale
